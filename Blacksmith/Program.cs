92bd905253c102a5b65011bf3969140faf66684f
ttodo <ttodo@DESKTOP-1JGF0DM>
2022-09-22 15:03:58 +0300

    Add project files.

---
+++ Blacksmith/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blacksmith
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> swordsTypeAndResurces = new Dictionary<string, int>() 
+            { 
+                { "Gladius", 70 },
+                { "Shamshir", 80},
+                { "Katana", 90},
+                { "Sabre", 110},
+                { "Broadsword", 150}
+            };
+            Dictionary<string, int> swordsCounter = new Dictionary<string, int>()
+            {
+                { "Gladius", 0 },
+                { "Shamshir", 0},
+                { "Katana", 0},
+                { "Sabre", 0},
+                { "Broadsword", 0}
+            };
+
+            Queue<int> steel = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Stack<int> carbon = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            while (steel.Count >0 && carbon.Count> 0) 
+            {
+                int currSteel = steel.Peek();
+                int currCarbon = carbon.Peek();
+                int sum = currSteel + currCarbon;
+
+                if (sum == swordsTypeAndResurces["Gladius"])
+                {
+                    swordsCounter["Gladius"]++;
+                    steel.Dequeue();
+                    carbon.Pop();
+                }
+                else if (sum == swordsTypeAndResurces["Shamshir"])
+                {
+                    swordsCounter["Shamshir"]++;
+                    steel.Dequeue();
+                    carbon.Pop();
+                }
+                else if (sum == swordsTypeAndResurces["Katana"])
+                {
+                    swordsCounter["Katana"]++;
+                    steel.Dequeue();
+                    carbon.Pop();
+                }
+                else if (sum == swordsTypeAndResurces["Sabre"])
+                {
+                    swordsCounter["Sabre"]++;
+                    steel.Dequeue();
+                    carbon.Pop();
+                }
+                else if (sum == swordsTypeAndResurces["Broadsword"])
+                {
+                    swordsCounter["Broadsword"]++;
+                    steel.Dequeue();
+                    carbon.Pop();
+                }
+                else
+                {
+                    steel.Dequeue();
+                    currCarbon += 5;
+                    carbon.Pop();
+                    carbon.Push(currCarbon);
+
+                }
+
+            }
+            int totalSwords = swordsCounter.Sum(s => s.Value);
+            if (totalSwords ==0)
+            {
+                Console.WriteLine("You did not have enough resources to forge a sword.");
+            }
+            else
+            {
+                Console.WriteLine($"You have forged {totalSwords} swords.");
+            }
+            if (steel.Count>0)
+            {
+                Console.WriteLine($"Steel left: {string.Join(", ", steel)}");
+            }
+            else
+            {
+                Console.WriteLine("Steel left: none");
+            }
+            if (carbon.Count > 0)
+            {
+                Console.WriteLine($"Carbon left: {string.Join(", ", carbon)}");
+            }
+            else
+            {
+                Console.WriteLine("Carbon left: none");
+            }
+            swordsCounter = swordsCounter.Where(s=>s.Value != 0).OrderBy(s => s.Key).ToDictionary(s=>s.Key, v => v.Value);
+            foreach (var sword in swordsCounter)
+            {
+                Console.WriteLine($"{sword.Key}: {sword.Value}");
+            }
+        }
+    }
+}
---
+++ TruffleHunter/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TruffleHunter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            int eatenByBoar = 0;
+            char[,] matrix = new char[matrixSize, matrixSize];
+            Dictionary<char, int> colectingTruffle = new Dictionary<char, int>()
+            {
+                { 'B',0 },
+                { 'S', 0},
+                { 'W',0}
+            };
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] inputRow = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputRow[col];
+                }
+
+            }
+            string commands = "";
+            while ((commands = Console.ReadLine()) != "Stop the hunt")
+            {
+                string[] tokens = commands.Split();
+                string action = tokens[0];
+                int row = int.Parse(tokens[1]);
+                int col = int.Parse(tokens[2]);
+
+                if (action == "Collect")
+                {
+                    char currPosition = matrix[row, col];
+                    if (currPosition == 'B')
+                    {
+                        colectingTruffle['B']++;
+                    }
+                    else if (currPosition == 'S')
+                    {
+                        colectingTruffle['S']++;
+                    }
+                    else if (currPosition == 'W')
+                    {
+                        colectingTruffle['W']++;
+                    }
+                    matrix[row, col] = '-';
+                }
+                else if (action == "Wild_Boar")
+                {
+                    string direction = tokens[3];
+                    char boarPosition = matrix[row, col];
+
+                    if (direction == "up")
+                    {
+
+                        for (int rows = row; rows >= 0; rows -= 2)
+                        {
+                            char currChar = matrix[rows, col];
+                            if (currChar == 'B'|| currChar == 'S'|| currChar == 'W')
+                            {
+                                eatenByBoar++;
+                                matrix[rows, col] = '-';
+                            }
+                        }
+                    }
+                    else if (direction == "down")
+                    {
+                        for (int rows = row; rows <matrix.GetLength(0); rows += 2)
+                        {
+                            char currChar = matrix[rows, col];
+                            if (currChar == 'B' || currChar == 'S' || currChar == 'W')
+                            {
+                                eatenByBoar++;
+                                matrix[rows, col] = '-';
+                            }
+                        }
+
+
+                    }
+                    else if (direction == "left")
+                    {
+                        for (int cols = col; cols >=0; cols -= 2)
+                        {
+                            char currChar = matrix[row, cols];
+                            if (currChar == 'B' || currChar == 'S' || currChar == 'W')
+                            {
+                                eatenByBoar++;
+                                matrix[row, cols] = '-';
+                            }
+                        }
+
+
+                    }
+                    else if (direction == "right")
+                    {
+                        for (int cols = col; cols < matrix.GetLength(1); cols += 2)
+                        {
+                            char currChar = matrix[row, cols];
+                            if (currChar == 'B' || currChar == 'S' || currChar == 'W')
+                            {
+                                eatenByBoar++;
+                                matrix[row, cols] = '-';
+                            }
+                        }
+
+                    }
+
+                }
+            }
+            Console.WriteLine($"Peter manages to harvest {colectingTruffle['B']} black, {colectingTruffle['S']} summer, and {colectingTruffle['W']} white truffles.");
+            Console.WriteLine($"The wild boar has eaten {eatenByBoar} truffles.");
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
+
+
+
